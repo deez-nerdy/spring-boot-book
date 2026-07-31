1bbe20927175e8bd7b65530283ff1238e7ccc186
Nikita Kravtsov <deez999deez@gmail.com>
2026-07-31 17:42:45 +0300

    Initial Spring Boot project

---
+++ src/main/java/spring/bookstore/springbootintro/SpringBootIntroApplication.java
@@ -0,0 +1,13 @@
+package spring.bookstore.springbootintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntroApplication.class, args);
+    }
+
+}
