7310a60d0517f5c0e5b70ddb9da8f5060caf4ee8
Nikita Kravtsov <deez999deez@gmail.com>
2026-08-04 11:56:28 +0300

    ADD: - @Table annotation to Book entity class;

--- src/main/java/spring/bookstore/springbootintro/model/Book.java
+++ src/main/java/spring/bookstore/springbootintro/model/Book.java
@@ -1,32 +1,30 @@
 package spring.bookstore.springbootintro.model;
 
-import jakarta.annotation.Nonnull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Nonnull
+    @Column(nullable = false)
     private String title;
-    @Nonnull
+    @Column(nullable = false)
     private String author;
-    @Nonnull
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String isbn;
-    @Nonnull
+    @Column(nullable = false)
     private BigDecimal price;
     private String description;
     private String coverImage;
