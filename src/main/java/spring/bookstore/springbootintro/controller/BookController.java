e5002c83f2ae8e63529d80003c0d7149c3eb60b1
Nikita Kravtsov <deez999deez@gmail.com>
2026-08-17 14:27:55 +0300

    Correct BookController annotation.

--- src/main/java/spring/bookstore/springbootintro/controller/BookController.java
+++ src/main/java/spring/bookstore/springbootintro/controller/BookController.java
@@ -2,11 +2,13 @@ package spring.bookstore.springbootintro.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import spring.bookstore.springbootintro.dto.BookDto;
 import spring.bookstore.springbootintro.dto.CreateBookRequestDto;
@@ -14,22 +16,22 @@ import spring.bookstore.springbootintro.service.BookService;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
-    @PostMapping("/api/books")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public BookDto createBook(@RequestBody CreateBookRequestDto createBookRequestDto) {
         return bookService.save(createBookRequestDto);
     }
 
-    @RequestMapping("/api/books/{id}")
-    @ResponseBody
+    @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getById(id);
     }
 
-    @RequestMapping("/api/books")
-    @ResponseBody
+    @GetMapping
     public List<BookDto> getAll() {
         return bookService.getAll();
     }
